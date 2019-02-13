196505da06ae9c93ea69bb5a64845890d78a3b12
Brian <brianhungerman@gmail.com>
2019-02-13 15:03:50 -0800

    Printing winner to console

--- Lab2/app_main.cpp
+++ Lab2/app_main.cpp
@@ -92,159 +92,175 @@ void appDrawScene()
     //check for a win::
     glColor3f(1.0, 0.0, 0.0);
     double center;
+    checkForWins();
+    bool placed = false;
+    if (singlePlayer && (turnNumber % 2 == 1))
+    {
+        for (int x = 0; x < 3; x++)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                if (board[x][y] == 0 && placed == false)
+                {
+                    board[x][y] = (turnNumber++) % 2 + 1;
+                    placed = true;
+                    break;
+                }
+            }
+        }
+    }
+    // bool isBoardFull = true;
+    // for (int x = 0; x < 3; x++)
+    // {
+    //     for (int y = 0; y < 3; y++)
+    //     {
+    //         if (board[x][y] == 0) {
+    //             isBoardFull = false;
+    //             break;
+    //         }
+    //     }
+    // }
+
+    // We have been drawing everything to the back buffer
+    // Swap the buffers to see the result of what we drew
+    glFlush();
+    glutSwapBuffers();
+}
+
+void checkForWins()
+{
+    int winner = 0;
     for (int i = 0; i < 3; i++)
     {
         center = i - 1; //-1, 0, 1
         center *= .66;  //-.66, 0, .66
         if (board[i][0] != 0 && board[i][0] == board[i][1] && board[i][1] == board[i][2])
         {
+            winner = board[i][0];
             glBegin(GL_LINES);
             glVertex3f(center, -1, 1);
             glVertex3f(center, 1, 1);
             glEnd();
-            //win
-	}
-            if (board[0][i] != 0 && board[0][i] == board[1][i] && board[1][i] == board[2][i])
-            {
-                glBegin(GL_LINES);
-                glVertex3f(1, center, 1);
-                glVertex3f(-1, center, 1);
-                glEnd();
-                //win
-            }
-    }
-        if (board[0][0] != 0 && board[0][0] == board[1][1] && board[1][1] == board[2][2])
+        }
+        if (board[0][i] != 0 && board[0][i] == board[1][i] && board[1][i] == board[2][i])
         {
+            winner = board[0][i];
             glBegin(GL_LINES);
-            glVertex3f(-1, -1, 1);
-            glVertex3f(1, 1, 1);
+            glVertex3f(1, center, 1);
+            glVertex3f(-1, center, 1);
             glEnd();
-            //win
         }
-        if (board[0][2] != 0 && board[0][2] == board[1][1] && board[1][1] == board[2][0])
+    }
+    if (board[0][0] != 0 && board[0][0] == board[1][1] && board[1][1] == board[2][2])
+    {
+        winner = board[0][0];
+        glBegin(GL_LINES);
+        glVertex3f(-1, -1, 1);
+        glVertex3f(1, 1, 1);
+        glEnd();
+    }
+    if (board[0][2] != 0 && board[0][2] == board[1][1] && board[1][1] == board[2][0])
+    {
+        winner = board[0][2];
+        glBegin(GL_LINES);
+        glVertex3f(-1, 1, 1);
+        glVertex3f(1, -1, 1);
+        glEnd();
+    }
+    if (winner != 0)
+    {
+        cout << "WINNER:\n" if (winner == 1)
         {
-            glBegin(GL_LINES);
-            glVertex3f(-1, 1, 1);
-            glVertex3f(1, -1, 1);
-            glEnd();
-            //win
+            cout << "X has won!\n";
         }
-	bool placed = false;
-        if (singlePlayer && (turnNumber % 2 == 1))
+        if (winner == 2)
         {
-            for (int x = 0; x < 3; x++)
-            {
-                for (int y = 0; y < 3; y++)
-                {
-                    if (board[x][y] == 0 && placed == false)
-                    {
-                        board[x][y] = (turnNumber++) % 2 + 1;
-			placed = true;
-			break;
-                    }
-                }
-            }
+            cout << "O has won!\n";
         }
-        // bool isBoardFull = true;
-        // for (int x = 0; x < 3; x++)
-        // {
-        //     for (int y = 0; y < 3; y++)
-        //     {
-        //         if (board[x][y] == 0) {
-        //             isBoardFull = false;
-        //             break;
-        //         }
-        //     }
-        // }
+    }
+}
+//-------------------------------------------------------
+// A function to convert window coordinates to scene
+// We use it when a mouse event is handled
+// Arguments:
+//	x, y - the coordinates to be updated
+//-------------------------------------------------------
+void windowToScene(float &x, float &y)
+{
+    x = (2.0f * (x / float(width))) - 1.0f;
+    y = 1.0f - (2.0f * (y / float(height)));
+}
 
-        // We have been drawing everything to the back buffer
-        // Swap the buffers to see the result of what we drew
-        glFlush();
-        glutSwapBuffers();
+//-------------------------------------------------------
+// A function to handle window resizing
+// Called every time the window is resized
+// Arguments:
+//	b    - mouse button that was clicked, left or right
+//	s 	 - state, either mouse-up or mouse-down
+//	x, y - coordinates of the mouse when click occured
+//-------------------------------------------------------
+void appReshapeFunc(int w, int h)
+{
+    // Window size has changed
+    width = w;
+    height = h;
+
+    double scale, center;
+    double winXmin, winXmax, winYmin, winYmax;
+
+    // Define x-axis and y-axis range
+    const double appXmin = -1.0;
+    const double appXmax = 1.0;
+    const double appYmin = -1.0;
+    const double appYmax = 1.0;
+
+    // Define that OpenGL should use the whole window for rendering
+    glViewport(0, 0, w, h);
+
+    // Set up the projection matrix using a orthographic projection that will
+    // maintain the aspect ratio of the scene no matter the aspect ratio of
+    // the window, and also set the min/max coordinates to be the disered ones
+    w = (w == 0) ? 1 : w;
+    h = (h == 0) ? 1 : h;
+
+    if ((appXmax - appXmin) / w < (appYmax - appYmin) / h)
+    {
+        scale = ((appYmax - appYmin) / h) / ((appXmax - appXmin) / w);
+        center = (appXmax + appXmin) / 2;
+        winXmin = center - (center - appXmin) * scale;
+        winXmax = center + (appXmax - center) * scale;
+        winYmin = appYmin;
+        winYmax = appYmax;
+    }
+    else
+    {
+        scale = ((appXmax - appXmin) / w) / ((appYmax - appYmin) / h);
+        center = (appYmax + appYmin) / 2;
+        winYmin = center - (center - appYmin) * scale;
+        winYmax = center + (appYmax - center) * scale;
+        winXmin = appXmin;
+        winXmax = appXmax;
     }
 
-    //-------------------------------------------------------
-    // A function to convert window coordinates to scene
-    // We use it when a mouse event is handled
-    // Arguments:
-    //	x, y - the coordinates to be updated
-    //-------------------------------------------------------
-    void windowToScene(float &x, float &y)
+    // Now we use glOrtho to set up our viewing frustum
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(winXmin, winXmax, winYmin, winYmax, -1, 1);
+}
+
+//-------------------------------------------------------
+// A function to handle mouse clicks
+// Called every time the mouse button goes up or down
+// Arguments:
+//	b    - mouse button that was clicked, left or right
+//	s 	 - state, either mouse-up or mouse-down
+//	x, y - coordinates of the mouse when click occured
+//-------------------------------------------------------
+void appMouseFunc(int button, int state, int x, int y)
+{
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
     {
-        x = (2.0f * (x / float(width))) - 1.0f;
-        y = 1.0f - (2.0f * (y / float(height)));
-    }
 
-    //-------------------------------------------------------
-    // A function to handle window resizing
-    // Called every time the window is resized
-    // Arguments:
-    //	b    - mouse button that was clicked, left or right
-    //	s 	 - state, either mouse-up or mouse-down
-    //	x, y - coordinates of the mouse when click occured
-    //-------------------------------------------------------
-    void appReshapeFunc(int w, int h)
-    {
-        // Window size has changed
-        width = w;
-        height = h;
-
-        double scale, center;
-        double winXmin, winXmax, winYmin, winYmax;
-
-        // Define x-axis and y-axis range
-        const double appXmin = -1.0;
-        const double appXmax = 1.0;
-        const double appYmin = -1.0;
-        const double appYmax = 1.0;
-
-        // Define that OpenGL should use the whole window for rendering
-        glViewport(0, 0, w, h);
-
-        // Set up the projection matrix using a orthographic projection that will
-        // maintain the aspect ratio of the scene no matter the aspect ratio of
-        // the window, and also set the min/max coordinates to be the disered ones
-        w = (w == 0) ? 1 : w;
-        h = (h == 0) ? 1 : h;
-
-        if ((appXmax - appXmin) / w < (appYmax - appYmin) / h)
-        {
-            scale = ((appYmax - appYmin) / h) / ((appXmax - appXmin) / w);
-            center = (appXmax + appXmin) / 2;
-            winXmin = center - (center - appXmin) * scale;
-            winXmax = center + (appXmax - center) * scale;
-            winYmin = appYmin;
-            winYmax = appYmax;
-        }
-        else
-        {
-            scale = ((appXmax - appXmin) / w) / ((appYmax - appYmin) / h);
-            center = (appYmax + appYmin) / 2;
-            winYmin = center - (center - appYmin) * scale;
-            winYmax = center + (appYmax - center) * scale;
-            winXmin = appXmin;
-            winXmax = appXmax;
-        }
-
-        // Now we use glOrtho to set up our viewing frustum
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(winXmin, winXmax, winYmin, winYmax, -1, 1);
-    }
-
-    //-------------------------------------------------------
-    // A function to handle mouse clicks
-    // Called every time the mouse button goes up or down
-    // Arguments:
-    //	b    - mouse button that was clicked, left or right
-    //	s 	 - state, either mouse-up or mouse-down
-    //	x, y - coordinates of the mouse when click occured
-    //-------------------------------------------------------
-    void appMouseFunc(int button, int state, int x, int y)
-    {
-        if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        
-    
         // Convert from Window to Scene coordinates
         float mx = (float)x;
         float my = (float)y;
@@ -259,90 +275,91 @@ void appDrawScene()
         my += 1.5;               //my is from 0 to 3
         int y_index = floor(my); //0-1 == 0, 1-2 == 1, 2-3 == 2 (index y of 3x3 matrix)
 
-	if (board[x_index][y_index] == 0) {
-	  board[x_index][y_index] = (turnNumber++ % 2) + 1; //Odd turns == Xs, even turns == Os
-	
-	  appDrawScene();
-	}
+        if (board[x_index][y_index] == 0)
+        {
+            board[x_index][y_index] = (turnNumber++ % 2) + 1; //Odd turns == Xs, even turns == Os
+
+            appDrawScene();
+        }
         // Redraw the scene by calling appDrawScene above
         // so that the point we added above will get painted
         glutPostRedisplay();
     }
-    }
+}
 
-    //-------------------------------------------------------
-    // A function to handle mouse dragging
-    // Called every time mouse moves while button held down
-    // Arguments:
-    //	x, y - current coordinates of the mouse
-    //-------------------------------------------------------
-    void appMotionFunc(int x, int y)
+//-------------------------------------------------------
+// A function to handle mouse dragging
+// Called every time mouse moves while button held down
+// Arguments:
+//	x, y - current coordinates of the mouse
+//-------------------------------------------------------
+void appMotionFunc(int x, int y)
+{
+
+    // Again, we redraw the scene
+    glutPostRedisplay();
+}
+
+//-------------------------------------------------------
+// A function to handle keyboard events
+// Called every time a key is pressed on the keyboard
+// Arguments:
+//	key  - the key that was pressed
+//	x, y - coordinates of the mouse when key is pressed
+//-------------------------------------------------------
+void appKeyboardFunc(unsigned char key, int x, int y)
+{
+
+    switch (key)
     {
-
-        // Again, we redraw the scene
-        glutPostRedisplay();
+    case 27:
+        exit(0);
+        break;
+    default:
+        break;
     }
 
-    //-------------------------------------------------------
-    // A function to handle keyboard events
-    // Called every time a key is pressed on the keyboard
-    // Arguments:
-    //	key  - the key that was pressed
-    //	x, y - coordinates of the mouse when key is pressed
-    //-------------------------------------------------------
-    void appKeyboardFunc(unsigned char key, int x, int y)
-    {
+    // After all the state changes, redraw the scene
+    glutPostRedisplay();
+}
 
-        switch (key)
-        {
-        case 27:
-            exit(0);
-            break;
-        default:
-            break;
-        }
+void idle()
+{
+}
 
-        // After all the state changes, redraw the scene
-        glutPostRedisplay();
-    }
+int main(int argc, char **argv)
+{
+    // Initialize GLUT
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
 
-    void idle()
-    {
-    }
+    // Setup window position, size, and title
+    glutInitWindowPosition(20, 60);
+    glutInitWindowSize(width, height);
+    glutCreateWindow("CSE165 Tic Tac Toe");
 
-    int main(int argc, char **argv)
-    {
-        // Initialize GLUT
-        glutInit(&argc, argv);
-        glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+    // Setup some OpenGL options
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_POINT_SMOOTH);
+    glEnable(GL_LINE_SMOOTH);
 
-        // Setup window position, size, and title
-        glutInitWindowPosition(20, 60);
-        glutInitWindowSize(width, height);
-        glutCreateWindow("CSE165 Tic Tac Toe");
+    // Set callback for drawing the scene
+    glutDisplayFunc(appDrawScene);
 
-        // Setup some OpenGL options
-        glEnable(GL_DEPTH_TEST);
-        glEnable(GL_POINT_SMOOTH);
-        glEnable(GL_LINE_SMOOTH);
+    // Set callback for resizing th window
+    glutReshapeFunc(appReshapeFunc);
 
-        // Set callback for drawing the scene
-        glutDisplayFunc(appDrawScene);
+    // Set callback to handle mouse clicks
+    glutMouseFunc(appMouseFunc);
 
-        // Set callback for resizing th window
-        glutReshapeFunc(appReshapeFunc);
+    // Set callback to handle mouse dragging
+    glutMotionFunc(appMotionFunc);
 
-        // Set callback to handle mouse clicks
-        glutMouseFunc(appMouseFunc);
+    // Set callback to handle keyboad events
+    glutKeyboardFunc(appKeyboardFunc);
 
-        // Set callback to handle mouse dragging
-        glutMotionFunc(appMotionFunc);
+    glutIdleFunc(idle);
 
-        // Set callback to handle keyboad events
-        glutKeyboardFunc(appKeyboardFunc);
-
-        glutIdleFunc(idle);
-
-        // Start the main loop
-        glutMainLoop();
-    }
+    // Start the main loop
+    glutMainLoop();
+}
